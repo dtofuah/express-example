212ab72bf2911aac855d24c2e6771211cdc992a2
Donald Tofuah <dtofuah@gmail.com>
2017-05-19 23:30:37 -0400

    country page added

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const app = express();
+const countries = require("./json/countries.json");
+console.log(countries);
 
 app.set("view engine", "ejs");
 
 app.use(express.static("assets"));
 
 
-app.get("/gallary", function(req, res) {
-	console.log("Seriving up Homepage....");
-	res.render("gallary", {
+function changeTemplate(res, pages, pageStuf) {
+	return res.render("template", {
+		pages: pages,
+		pageStuf: pageStuf,
 		header: "Donald Tofuah",
 	});
+}
+
+app.get("/country", function(req, res) {
+	console.log("Serving up country page");
+	var country = countries[req.params.code];
+	console.log(countries);
+
+	if (!country) {
+		res.status(404);
+		return res.send("This code is not recognized");
+	}
+
+	res.render("page/country", "Contry", country);
+});
+
+app.get("/gallary", function(req, res) {
+	console.log("Seriving up Gallary Page....");
+	changeTemplate(res, "gallary");
 });
 
 app.get("/", function(req, res) {
 	console.log("Seriving up Homepage....");
-	res.render("index");
+	changeTemplate(res, "index");
 });
 
 
